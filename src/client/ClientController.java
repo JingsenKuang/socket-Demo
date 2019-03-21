9962599643b6decb7e20202a75e5d25af16d9dfa
JingSenKuang <654726432@qq.com>
2019-03-22 00:23:08 +0800

    change Send way

--- src/client/ClientController.java
+++ src/client/ClientController.java
@@ -19,17 +19,24 @@ public class ClientController {
     private TextArea txta_SendMessage;
     @FXML
     private Button btn_Send;
-    public void btn_SendAction() throws IOException {
+    private Socket socket;
+    public void connectServer() throws IOException {
         String ipAddress = txtf_IpAddress.getText();
         String port = txtf_Port.getText();
-        try (Socket socket = new Socket(ipAddress,Integer.parseInt(port))){
-            socket.setSoTimeout(15000);
+        socket = new Socket(ipAddress,Integer.parseInt(port));
+       // socket.setSoTimeout(15000);
+    }
+    public void btn_SendAction() throws IOException {
+        try{
             OutputStream out = socket.getOutputStream();
             Writer writer = new OutputStreamWriter(out,"UTF-8");
             writer = new BufferedWriter(writer);
-            writer.write(txta_SendMessage.getText());
+            writer.write(txta_SendMessage.getText()+"\n");
             writer.flush();
             System.out.println(txta_SendMessage.getText());
+        }
+        finally {
+
         }
     }
 
--- src/client/Main.java
+++ src/client/Main.java
@@ -1,19 +1,40 @@
 package client;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.net.URL;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("client.fxml"));
+        URL location = getClass().getResource("client.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(location);
+        fxmlLoader.setBuilderFactory(new JavaFXBuilderFactory());
+        Parent root = fxmlLoader.load();
+        //Parent root = FXMLLoader.load(getClass().getResource("client.fxml"));
         primaryStage.setTitle("client");
         primaryStage.setScene(new Scene(root, 531, 366));
         primaryStage.show();
+        ClientController controller = fxmlLoader.getController();
+        controller.connectServer();
+
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                System.out.print("监听到窗口关闭");
+            }
+        });
+
     }
 
 
